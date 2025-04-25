5582fe5b86fccf9dad3252c6ed37c08c1b00734f
jasper.schepers <jasper.schepers@gmail.com>
2025-04-25 14:21:31 +0200

    Begonnen met Jamaica

--- Flags/jamaica.py
+++ Flags/jamaica.py
@@ -1,72 +1,53 @@
+import math
 from turtle import *
-from shapes import polygon
+from shapes import polygon, rect
+
+
+
 penup()
 bgcolor("gray")
-blue = "#00205B"
-red = "#BA0C2F"
-scale = 10
+black = "#000000"
+green = "#007847"
+yellow = "#FFB915"
+a = math.sqrt(0.5**2 + 0.25**2)
+a = math.sqrt(1**2 + 0.5**2)
+scale = 50
+
+flag_width_on_plan = 12
+flag_height_on_plan = 6
+
+
 
 speed(0)
+fillcolor(black)
+rect( 0, 0, flag_width_on_plan*scale, flag_height_on_plan*scale)
 
 hoekpunten = [
         Vec2D(0,0),
-        Vec2D(0, 6),
-        Vec2D(16, 6),
-        Vec2D(27, 0),
+        Vec2D(flag_width_on_plan/2, flag_height_on_plan/2),
+        Vec2D(flag_width_on_plan, 0),
         Vec2D(0, 0)
 ]
-fillcolor(red)
-polygon(hoekpunten, scale, Vec2D(-200, -100))
-Vec2D(0, 0)
+fillcolor(green)
+polygon(hoekpunten, scale)
 
 hoekpunten = [
-        Vec2D(0, 10),
-        Vec2D(0, 16),
-        Vec2D(27, 16),
-        Vec2D(16, 10),
-        Vec2D(0, 10)
-    ]
-fillcolor(red)
-polygon(hoekpunten, scale, Vec2D(-200, -100))
-hoekpunten = [
-        Vec2D(0, 6),
-        Vec2D(0, 10),
-        Vec2D(16, 10),
-        Vec2D(27, 8),
-        Vec2D(16, 6),
-        Vec2D(0, 6)
-    ]
-fillcolor("white")
-polygon(hoekpunten, scale, Vec2D(-200, -100))
+        Vec2D(0,flag_height_on_plan),
+        Vec2D(flag_width_on_plan/2, flag_height_on_plan/2),
+        Vec2D(flag_width_on_plan, flag_height_on_plan),
+        Vec2D(0, flag_height_on_plan)
+]
+fillcolor(green)
+polygon(hoekpunten, scale)
 
 hoekpunten = [
-        Vec2D(6, 16),
-        Vec2D(10, 16),
-        Vec2D(10, 0),
-        Vec2D(6, 0),
-        Vec2D(6, 16),
-    ]
-fillcolor("white")
-polygon(hoekpunten, scale, Vec2D(-200, -100))
-hoekpunten = [
-        Vec2D(0, 7),
-        Vec2D(0, 9),
-        Vec2D(21.5, 9),
-        Vec2D(27, 8),
-        Vec2D(21.5, 7),
-        Vec2D(0, 7)
-    ]
-fillcolor(blue)
-polygon(hoekpunten, scale, Vec2D(-200, -100))
-hoekpunten = [
-        Vec2D(7, 16),
-        Vec2D(9, 16),
-        Vec2D(9, 0),
-        Vec2D(7, 0),
-        Vec2D(7, 16),
-    ]
-fillcolor(blue)
-polygon(hoekpunten, scale, Vec2D(-200, -100))
+        Vec2D(0,flag_height_on_plan),
+        Vec2D(flag_width_on_plan/2, flag_height_on_plan/2),
+        Vec2D(flag_width_on_plan, flag_height_on_plan),
+        Vec2D(0, flag_height_on_plan)
+]
+fillcolor(green)
+polygon(hoekpunten, scale)
 
 
 done()
